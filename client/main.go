b59d03f0fd1e609571afe6a74e967bd194043244
Martin Bocanegra <martinbocanegra01@gmail.com>
2023-10-23 20:34:25 -0300

    formatted file

--- client/main.go
+++ client/main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
-	"strings"
+  "bufio"
+  "fmt"
+  "net"
+  "os"
+  "strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer conn.Close()
+  conn, err := net.Dial("tcp", "localhost:8080")
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  defer conn.Close()
 
-	go handleRead(conn)
-	handleWrite(conn)
+  go handleRead(conn)
+  handleWrite(conn)
 }
 
 func handleRead(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-	for {
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Closed connection:", err)
-			return
-		}
-		fmt.Print("Message received: " + message + "> ")
-	}
+  reader := bufio.NewReader(conn)
+  for {
+    message, err := reader.ReadString('\n')
+    if err != nil {
+      fmt.Println("Closed connection:", err)
+      return
+    }
+    fmt.Print("Message received: " + message)
+    fmt.Print("> ")
+  }
 }
 
 func handleWrite(conn net.Conn) {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("> ")
-		message, _ := reader.ReadString('\n')
-		message = strings.TrimSpace(message)
-		if message == "exit" {
-			return
-		}
-		conn.Write([]byte(message + "\n"))
-	}
+  reader := bufio.NewReader(os.Stdin)
+  for {
+    fmt.Print("> ")
+    message, _ := reader.ReadString('\n')
+    message = strings.TrimSpace(message)
+    if message == "exit" {
+      return
+    }
+    conn.Write([]byte(message + "\n"))
+  }
 }
