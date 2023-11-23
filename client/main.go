7e85aa4719a6132f222964ec69092489961aca75
Martin Bocanegra <martinbocanegra01@gmail.com>
2023-11-23 11:53:16 -0300

    parse keyboard events and send to server (WIP)

--- client/main.go
+++ client/main.go
@@ -1,47 +1,72 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "net"
-  "os"
-  "strings"
+	"bufio"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/nsf/termbox-go"
 )
 
 func main() {
-  conn, err := net.Dial("tcp", "localhost:8080")
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  defer conn.Close()
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-  go handleRead(conn)
-  handleWrite(conn)
-}
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
-func handleRead(conn net.Conn) {
-  reader := bufio.NewReader(conn)
-  for {
-    message, err := reader.ReadString('\n')
-    if err != nil {
-      fmt.Println("Closed connection:", err)
-      return
-    }
-    fmt.Print("Message received: " + message)
-    fmt.Print("> ")
-  }
-}
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
 
-func handleWrite(conn net.Conn) {
-  reader := bufio.NewReader(os.Stdin)
-  for {
-    fmt.Print("> ")
-    message, _ := reader.ReadString('\n')
-    message = strings.TrimSpace(message)
-    if message == "exit" {
-      return
-    }
-    conn.Write([]byte(message + "\n"))
-  }
+	// Goroutine para manejar los mensajes del servidor
+	go func() {
+		reader := bufio.NewReader(conn)
+		for {
+			message, err := reader.ReadString('\n')
+			if err != nil {
+				// Manejar error
+				break
+			}
+
+			if strings.HasPrefix(message, SRV_GAME_UPDATE) {
+				// Parsear y actualizar el estado del juego
+				// Ejemplo: GAME_UPDATE:player1Pos,player2Pos,ballX,ballY
+				parts := strings.Split(message, ":")
+				gameUpdate := strings.Split(parts[1], ",")
+				if len(gameUpdate) == 4 {
+					player1Pos, _ := strconv.Atoi(gameUpdate[0])
+					player2Pos, _ := strconv.Atoi(gameUpdate[1])
+					ballX, _ := strconv.Atoi(gameUpdate[2])
+					ballY, _ := strconv.Atoi(gameUpdate[3])
+					DrawPongInterface(player1Pos, player2Pos, ballX, ballY)
+				}
+
+			}
+		}
+	}()
+
+	for {
+		ev := <-eventQueue
+		if ev.Type == termbox.EventKey {
+			if ev.Key == termbox.KeyArrowUp {
+				// Enviar acción de flecha hacia arriba al servidor
+				conn.Write([]byte("CLIENT_ACTION:ARROW_UP\n"))
+			}
+			if ev.Key == termbox.KeyEsc {
+				return
+			}
+
+		}
+	}
 }
