47b5e608447c06724d353f874930a709378dab9f
Martin Bocanegra <martinbocanegra01@gmail.com>
2023-11-23 11:53:40 -0300

    formatted file

--- server/main.go
+++ server/main.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "os"
-    "sync"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"sync"
 )
 
 var (
-    clients    = make(map[net.Conn]bool)
-    clientsMtx sync.Mutex
+	clients    = make(map[net.Conn]bool)
+	clientsMtx sync.Mutex
 )
 
 func main() {
-    server, err := net.Listen("tcp", ":8080")
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    defer server.Close()
+	server, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer server.Close()
 
-    for {
-        conn, err := server.Accept()
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
-        clientsMtx.Lock()
-        clients[conn] = true
-        clientsMtx.Unlock()
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		clientsMtx.Lock()
+		clients[conn] = true
+		clientsMtx.Unlock()
 
-        fmt.Println("New client: " + conn.RemoteAddr().String())
-        go handleClient(conn)
-    }
+		fmt.Println("New client: " + conn.RemoteAddr().String())
+		go handleClient(conn)
+	}
 }
 
 func handleClient(conn net.Conn) {
-    defer conn.Close()
-    ip := conn.RemoteAddr().String()
-    conn.Write([]byte("Welcome " + ip + "!\n"))
-    reader := bufio.NewReader(conn)
-    for {
-        message, err := reader.ReadString('\n')
-        if err != nil {
-            clientsMtx.Lock()
-            delete(clients, conn)
-            clientsMtx.Unlock()
-            break
-        }
+	defer conn.Close()
+	ip := conn.RemoteAddr().String()
+	conn.Write([]byte("Welcome " + ip + "!\n"))
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			clientsMtx.Lock()
+			delete(clients, conn)
+			clientsMtx.Unlock()
+			break
+		}
 
-        clientsMtx.Lock()
-        for client := range clients {
-            if client != conn {
-                client.Write([]byte(message))
-            }
-        }
-        clientsMtx.Unlock()
-    }
+		clientsMtx.Lock()
+		for client := range clients {
+			if client != conn {
+				client.Write([]byte(message))
+			}
+		}
+		clientsMtx.Unlock()
+	}
 }
